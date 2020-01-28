f48936df5c9195b85f81a55c1b2f57504c8112c4
Emilee Demore <emileedemore@outlook.com>
2020-01-28 16:56:06 -0600

    Added props to CustomerIdentityUser

---
+++ Controllers/CustomIdentityUsersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KenkoApp.Data;
+using KenkoApp.Models;
+
+namespace KenkoApp.Controllers
+{
+    public class CustomIdentityUsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomIdentityUsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CustomIdentityUsers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: CustomIdentityUsers/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customIdentityUser = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customIdentityUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(customIdentityUser);
+        }
+
+        // GET: CustomIdentityUsers/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: CustomIdentityUsers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,DateofBirth,Gender,SocialSecurityNumber,Email,Phone,SecondaryPhone,Address,City,State,ZipCode,MaritalStatus,EmergencyContact,Relationship,InsuranceProvider,InsurancePolicyNumber")] CustomIdentityUser customIdentityUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customIdentityUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customIdentityUser);
+        }
+
+        // GET: CustomIdentityUsers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customIdentityUser = await _context.Users.FindAsync(id);
+            if (customIdentityUser == null)
+            {
+                return NotFound();
+            }
+            return View(customIdentityUser);
+        }
+
+        // POST: CustomIdentityUsers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,FirstName,LastName,DateofBirth,Gender,SocialSecurityNumber,Email,Phone,SecondaryPhone,Address,City,State,ZipCode,MaritalStatus,EmergencyContact,Relationship,InsuranceProvider,InsurancePolicyNumber")] CustomIdentityUser customIdentityUser)
+        {
+            if (id != customIdentityUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customIdentityUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomIdentityUserExists(customIdentityUser.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customIdentityUser);
+        }
+
+        // GET: CustomIdentityUsers/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customIdentityUser = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customIdentityUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(customIdentityUser);
+        }
+
+        // POST: CustomIdentityUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customIdentityUser = await _context.Users.FindAsync(id);
+            _context.Users.Remove(customIdentityUser);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomIdentityUserExists(string id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+    }
+}
--- Models/CustomIdentityUser.cs
+++ Models/CustomIdentityUser.cs
@@ -7,12 +7,26 @@ using System.Threading.Tasks;
 
 namespace KenkoApp.Models
 {
-    public class CustomIdentityUser:IdentityUser  
+    public class CustomIdentityUser : IdentityUser  
 
     {
         //Emilee, add new patient user properties 
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string Gender { get; set; }
+        public string SocialSecurityNumber { get; set; }
+        public string SecondaryPhone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string MaritalStatus { get; set; }
+        public string EmergencyContact { get; set; }
+        public string Relationship { get; set; }
+        public string InsuranceProvider { get; set; }
+        public string InsurancePolicyNumber { get; set; }
+
 
     }
 }
