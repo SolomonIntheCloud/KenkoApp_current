d8722fd3f515d85f15c2bbed0e6834a390fd938b
Emilee Demore <emileedemore@outlook.com>
2020-01-28 21:50:24 -0600

    Added new patient user props to CustomIdentityUser.cs

--- Models/CustomIdentityUser.cs
+++ Models/CustomIdentityUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,20 +12,49 @@ namespace KenkoApp.Models
 
     {
         //Emilee, add new patient user properties 
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        
+        [Display(Name = "Date of Birth")]
         public DateTime DateofBirth { get; set; }
+        
+        [Display(Name = "Gender")]
         public string Gender { get; set; }
+
+        [Display(Name = "Social Security Number")]
         public string SocialSecurityNumber { get; set; }
+
+        [Display(Name = "Secondary Phone")]
         public string SecondaryPhone { get; set; }
+
+        [Display(Name = "Address")]
         public string Address { get; set; }
+
+        [Display(Name = "City")]
         public string City { get; set; }
+
+        [Display(Name = "State")]
         public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
         public string ZipCode { get; set; }
+
+        [Display(Name = "Marital Status")]
         public string MaritalStatus { get; set; }
+
+        [Display(Name = "Emergency Contact")]
         public string EmergencyContact { get; set; }
+
+        [Display(Name = "Relationship")]
         public string Relationship { get; set; }
+
+        [Display(Name = "Insurance Provider")]
         public string InsuranceProvider { get; set; }
+
+        [Display(Name = "Insurance Policy Number")]
         public string InsurancePolicyNumber { get; set; }
 
 
